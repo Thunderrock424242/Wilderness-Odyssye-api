3a1609215ed29a37524d0cb4894a19d3541515cf
Thunderrock424242 <masonjoshua8@gmail.com>
2024-06-22 23:46:54 -0400

    added some small things

---
+++ src/main/java/net/mcreator/wildernessoddesyapi/ModlList.java
@@ -0,0 +1,46 @@
+/*
+ * The code of this mod element is always locked.
+ *
+ * You can register new events in this class too.
+ *
+ * If you want to make a plain independent class, create it using
+ * Project Browser -> New... and make sure to make the class
+ * outside net.mcreator.wildernessoddesyapi as this package is managed by MCreator.
+ *
+ * If you change workspace package, modid or prefix, you will need
+ * to manually adapt this file to these changes or remake it.
+ *
+ * This class will be added in the mod root package.
+*/
+package net.mcreator.wildernessoddesyapi;
+
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.api.distmarker.Dist;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModlList {
+	public ModlList() {
+	}
+
+	@SubscribeEvent
+	public static void init(FMLCommonSetupEvent event) {
+		new ModlList();
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	@SubscribeEvent
+	public static void clientLoad(FMLClientSetupEvent event) {
+	}
+
+	@Mod.EventBusSubscriber
+	private static class ForgeBusEvents {
+		@SubscribeEvent
+		public static void serverLoad(ServerStartingEvent event) {
+		}
+	}
+}
