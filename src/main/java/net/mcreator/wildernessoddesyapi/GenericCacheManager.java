f59922acb51040fb1bd72f0c8e052391cf4387ee
Thunderrock424242 <masonjoshua8@gmail.com>
2024-06-25 17:08:05 -0400

    cleaned up code and fixed 1 error more to go

--- src/main/java/net/mcreator/wildernessoddesyapi/GenericCacheManager.java
+++ src/main/java/net/mcreator/wildernessoddesyapi/GenericCacheManager.java
@@ -1,17 +1,4 @@
-/*
- * The code of this mod element is always locked.
- *
- * You can register new events in this class too.
- *
- * If you want to make a plain independent class, create it using
- * Project Browser -> New... and make sure to make the class
- * outside net.mcreator.wildernessoddesyapi as this package is managed by MCreator.
- *
- * If you change workspace package, modid or prefix, you will need
- * to manually adapt this file to these changes or remake it.
- *
- * This class will be added in the mod root package.
-*/
+
 package net.mcreator.wildernessoddesyapi;
 
 import java.lang.ref.WeakReference;
@@ -20,16 +7,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
-
-// Generic Cache Manager
 class GenericCacheManager<K, V> {
     private final Map<K, WeakReference<V>> cache = new ConcurrentHashMap<>();
     private final ExecutorService cacheCleaner = Executors.newSingleThreadExecutor();
@@ -59,7 +36,6 @@ class GenericCacheManager<K, V> {
     }
 }
 
-// Custom Item Data Class
 class CustomItemData {
     private final String itemName;
     private final int itemValue;
@@ -78,7 +54,6 @@ class CustomItemData {
     }
 }
 
-// Custom Player Data Class
 class CustomPlayerData {
     private final String playerName;
     private final int playerLevel;
@@ -97,7 +72,6 @@ class CustomPlayerData {
     }
 }
 
-// Custom Block Data Class
 class CustomBlockData {
     private final String blockType;
     private final int blockHardness;
@@ -116,7 +90,6 @@ class CustomBlockData {
     }
 }
 
-// Custom Biome Data Class
 class CustomBiomeData {
     private final String biomeName;
     private final float temperature;
@@ -141,7 +114,6 @@ class CustomBiomeData {
     }
 }
 
-// Custom World Gen Data Class
 class CustomWorldGenData {
     private final String structureName;
     private final int complexity;
@@ -160,7 +132,6 @@ class CustomWorldGenData {
     }
 }
 
-// Custom Configuration Data Class
 class CustomConfigData {
     private final String configName;
     private final String configValue;
@@ -179,7 +150,6 @@ class CustomConfigData {
     }
 }
 
-// Custom Pathfinding Data Class
 class CustomPathfindingData {
     private final String entityId;
     private final BlockPos[] path;
@@ -198,7 +168,6 @@ class CustomPathfindingData {
     }
 }
 
-// Custom Weather Event Data Class
 class CustomWeatherData {
     private final String weatherType;
     private final long duration;
@@ -220,96 +189,5 @@ class CustomWeatherData {
 
     public boolean isSevere() {
         return isSevere;
-    }
-}
-
-// Custom Item Class
-public class CustomItem extends Item {
-    private static final GenericCacheManager<String, CustomItemData> itemCache = new GenericCacheManager<>();
-    private static final GenericCacheManager<String, CustomPlayerData> playerCache = new GenericCacheManager<>();
-    private static final GenericCacheManager<BlockPos, CustomBlockData> blockCache = new GenericCacheManager<>();
-    private static final GenericCacheManager<String, CustomBiomeData> biomeCache = new GenericCacheManager<>();
-    private static final GenericCacheManager<String, CustomWorldGenData> worldGenCache = new GenericCacheManager<>();
-    private static final GenericCacheManager<String, CustomConfigData> configCache = new GenericCacheManager<>();
-    private static final GenericCacheManager<String, CustomPathfindingData> pathfindingCache = new GenericCacheManager<>();
-    private static final GenericCacheManager<String, CustomWeatherData> weatherCache = new GenericCacheManager<>();
-
-    public CustomItem(Properties properties) {
-        super(properties);
-    }
-
-    @Override
-    public void onCraftedBy(ItemStack stack, Level world, Player player) {
-        super.onCraftedBy(stack, world, player);
-        
-        // Store custom item data in the cache
-        CustomItemData itemData = new CustomItemData(stack.getHoverName().getString(), calculateItemValue(stack));
-        itemCache.put(stack.getDescriptionId(), itemData);
-        
-        // Store custom player data in the cache
-        CustomPlayerData playerData = new CustomPlayerData(player.getName().getString(), player.experienceLevel);
-        playerCache.put(player.getStringUUID(), playerData);
-    }
-
-    @Override
-    public void inventoryTick(ItemStack stack, Level world, Entity entity, int slot, boolean selected) {
-        super.inventoryTick(stack, world, entity, slot, selected);
-
-        // Retrieve custom item data from the cache
-        CustomItemData itemData = itemCache.get(stack.getDescriptionId());
-        if (itemData != null) {
-            // Use the custom item data
-            applyCustomEffects(entity, itemData);
-        }
-
-        // Example of using block data
-        BlockPos pos = entity.blockPosition();
-        CustomBlockData blockData = blockCache.get(pos);
-        if (blockData != null) {
-            // Use custom block data (placeholder logic)
-            System.out.println("Block Type: " + blockData.getBlockType() + ", Hardness: " + blockData.getBlockHardness());
-        }
-    }
-
-    private int calculateItemValue(ItemStack stack) {
-        // Custom logic to calculate item value
-        return stack.getCount() * 10;
-    }
-
-    private void applyCustomEffects(Entity entity, CustomItemData data) {
-        // Custom logic to apply effects based on item data
-        if (entity instanceof Player) {
-            ((Player) entity).addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 100, data.getItemValue() / 10));
-        }
-    }
-
-    public static void addBlockData(BlockPos pos, String blockType, int hardness) {
-        CustomBlockData blockData = new CustomBlockData(blockType, hardness);
-        blockCache.put(pos, blockData);
-    }
-
-    public static void addBiomeData(String biomeId, String biomeName, float temperature, float humidity) {
-        CustomBiomeData biomeData = new CustomBiomeData(biomeName, temperature, humidity);
-        biomeCache.put(biomeId, biomeData);
-    }
-
-    public static void addWorldGenData(String structureId, String structureName, int complexity) {
-        CustomWorldGenData worldGenData = new CustomWorldGenData(structureName, complexity);
-        worldGenCache.put(structureId, worldGenData);
-    }
-
-    public static void addConfigData(String configId, String configName, String configValue) {
-        CustomConfigData configData = new CustomConfigData(configName, configValue);
-        configCache.put(configId, configData);
-    }
-
-    public static void addPathfindingData(String entityId, BlockPos[] path) {
-        CustomPathfindingData pathfindingData = new CustomPathfindingData(entityId, path);
-        pathfindingCache.put(entityId, pathfindingData);
-    }
-
-    public static void addWeatherData(String weatherId, String weatherType, long duration, boolean isSevere) {
-        CustomWeatherData weatherData = new CustomWeatherData(weatherType, duration, isSevere);
-        weatherCache.put(weatherId, weatherData);
     }
 }
