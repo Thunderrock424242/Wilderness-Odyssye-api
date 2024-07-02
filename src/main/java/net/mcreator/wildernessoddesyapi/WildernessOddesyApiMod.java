7fea82d9c759631222dc4169428f92fe79164286
Thunderrock424242 <masonjoshua8@gmail.com>
2024-07-01 20:41:46 -0400

    cleaned up code

--- src/main/java/net/mcreator/wildernessoddesyapi/WildernessOddesyApiMod.java
+++ src/main/java/net/mcreator/wildernessoddesyapi/WildernessOddesyApiMod.java
@@ -18,9 +18,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.network.FriendlyByteBuf;
 
-import net.mcreator.wildernessoddesyapi.init.WildernessOddesyApiModTabs;
 import net.mcreator.wildernessoddesyapi.init.WildernessOddesyApiModSounds;
-import net.mcreator.wildernessoddesyapi.init.WildernessOddesyApiModItems;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.Map;
@@ -40,10 +38,6 @@ public class WildernessOddesyApiMod {
 		NeoForge.EVENT_BUS.register(this);
 		modEventBus.addListener(this::registerNetworking);
 		WildernessOddesyApiModSounds.REGISTRY.register(modEventBus);
-
-		WildernessOddesyApiModItems.register(modEventBus);
-
-		WildernessOddesyApiModTabs.REGISTRY.register(modEventBus);
 
 		// Start of user code block mod init
 		// End of user code block mod init
