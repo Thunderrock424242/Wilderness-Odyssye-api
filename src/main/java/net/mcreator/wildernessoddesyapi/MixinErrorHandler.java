15723625367eeefa1ec258a5342ba233f7d3cc31
Thunderrock424242 <masonjoshua8@gmail.com>
2024-07-01 20:23:48 -0400

    chaanged some things

--- src/main/java/net/mcreator/wildernessoddesyapi/MixinErrorHandler.java
+++ src/main/java/net/mcreator/wildernessoddesyapi/MixinErrorHandler.java
@@ -1,39 +1,22 @@
 package net.mcreator.wildernessoddesyapi;
 
-import org.spongepowered.asm.mixin.extensibility.IMixinConfig;
-import org.spongepowered.asm.mixin.extensibility.IMixinErrorHandler;
-import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
-import org.spongepowered.asm.mixin.throwables.MixinApplyError;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.IEventBus;
 
-public class MixinErrorHandler implements IMixinErrorHandler {
-    private static final Logger LOGGER = LogManager.getLogger(MixinErrorHandler.class);
+@Mod("wilderness_oddesy_api")
+public class MixinErrorHandler {
+    public static final String MODID = "wilderness_oddesy_api";
 
-    @Override
-    public ErrorAction onPrepareError(IMixinConfig config, Throwable th, IMixinInfo mixinInfo, ErrorAction action) {
-        LOGGER.error("Mixin prepare error: {}", th.getMessage());
-        LOGGER.error("Mixin config: {}", config.getName());
-        LOGGER.error("Mixin: {}", mixinInfo.getName());
-        LOGGER.error("Cause: ", th);
-        return ErrorAction.ERROR;
+    public MixinErrorHandler() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.register(this);
     }
 
-    @Override
-    public ErrorAction onApplyError(String targetClassName, Throwable targetError, IMixinInfo mixinInfo, ErrorAction action) {
-        LOGGER.error("Mixin apply error: {}", targetError.getMessage());
-        LOGGER.error("Target class: {}", targetClassName);
-        LOGGER.error("Mixin: {}", mixinInfo.getName());
-        LOGGER.error("Cause: ", targetError);
-        return ErrorAction.ERROR;
-    }
-
-    @Override
-    public ErrorAction onError(String mixinClassName, Throwable th, IMixinInfo mixinInfo, ErrorAction action) {
-        LOGGER.error("Mixin error: {}", th.getMessage());
-        LOGGER.error("Mixin class: {}", mixinClassName);
-        LOGGER.error("Mixin: {}", mixinInfo.getName());
-        LOGGER.error("Cause: ", th);
-        return ErrorAction.ERROR;
+    @SubscribeEvent
+    public void setup(FMLCommonSetupEvent event) {
+        MixinErrorLogger.logInfo("Mixin Error Logger Mod Initialized");
     }
 }
--- src/main/java/net/mcreator/wildernessoddesyapi/MixinErrorLogger.java
+++ src/main/java/net/mcreator/wildernessoddesyapi/MixinErrorLogger.java
@@ -1,33 +1,16 @@
 package net.mcreator.wildernessoddesyapi;
 
-import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.bus.api.IEventBus;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-import org.spongepowered.asm.mixin.MixinEnvironment;
-import org.spongepowered.asm.mixin.extensibility.IMixinErrorHandler;
 
-@Mod(MixinErrorLogger.MODID)
 public class MixinErrorLogger {
-    public static final String MODID = "mixinerrorlogger";
-    public static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger(MixinErrorLogger.class);
 
-    public MixinErrorLogger() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        modEventBus.register(this);
-        initMixinErrorHandler();
+    public static void logError(String message, Throwable throwable) {
+        LOGGER.error(message, throwable);
     }
 
-    private void initMixinErrorHandler() {
-        // Programmatically set the error handler if necessary, or set it via the mixin config JSON
-        MixinEnvironment.getDefaultEnvironment().registerErrorHandlerClass(MixinErrorHandler.class.getName());
-    }
-
-    @SubscribeEvent
-    public void setup(FMLCommonSetupEvent event) {
-        LOGGER.info("Mixin Error Logger Mod Initialized");
+    public static void logInfo(String message) {
+        LOGGER.info(message);
     }
 }
