c396442f24d82b1471d8d1cd3887f4d375e4533c
Thunderrock424242 <masonjoshua8@gmail.com>
2024-07-27 20:29:45 -0400

    added geckolib support

--- src/main/java/net/mcreator/wildernessoddesyapi/init/WildernessOddesyApiModItems.java
+++ src/main/java/net/mcreator/wildernessoddesyapi/init/WildernessOddesyApiModItems.java
@@ -8,12 +8,13 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.DeferredHolder;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.core.registries.BuiltInRegistries;
 
 import net.mcreator.wildernessoddesyapi.item.FireclanSpiritItem;
 import net.mcreator.wildernessoddesyapi.WildernessOddesyApiMod;
 
 public class WildernessOddesyApiModItems {
-	public static final DeferredRegister.Items REGISTRY = DeferredRegister.createItems(WildernessOddesyApiMod.MODID);
+	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(BuiltInRegistries.ITEM, WildernessOddesyApiMod.MODID);
 	public static final DeferredHolder<Item, Item> FIRECLAN_SPIRIT = REGISTRY.register("fireclan_spirit", FireclanSpiritItem::new);
 	// Start of user code block custom items
 	// End of user code block custom items
