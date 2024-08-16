f4fd631cdc0a05f73a0f714ed3a3b39124a31460
Thunderrock424242 <masonjoshua8@gmail.com>
2024-08-15 22:25:01 -0400

    fixed some imports

--- src/main/java/net/mcreator/wildernessoddesyapi/LifecycleEvents.java
+++ src/main/java/net/mcreator/wildernessoddesyapi/LifecycleEvents.java
@@ -3,8 +3,8 @@ package net.mcreator.wildernessoddesyapi;
 
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.server.FMLServerStartingEvent;
-import net.neoforged.fml.event.server.FMLServerStoppingEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.event.server.ServerStoppingEvent;
 
 @Mod.EventBusSubscriber(modid = MyMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class LifecycleEvents {
@@ -12,12 +12,12 @@ public class LifecycleEvents {
     private static AssetCacheManager cacheManager;
 
     @SubscribeEvent
-    public static void onServerStarting(FMLServerStartingEvent event) {
+    public static void onServerStarting(ServerStartingEvent event) {
         cacheManager = new AssetCacheManager();
     }
 
     @SubscribeEvent
-    public static void onServerStopping(FMLServerStoppingEvent event) {
+    public static void onServerStopping(ServerStoppingEvent event) {
         cacheManager.shutdown();
     }
 }
--- src/main/java/net/mcreator/wildernessoddesyapi/ModInitializer.java
+++ src/main/java/net/mcreator/wildernessoddesyapi/ModInitializer.java
@@ -8,11 +8,11 @@ import net.minecraft.server.packs.repository.RepositorySource;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.ResourceProvider;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.client.event.RegisterClientReloadListenersEvent;
+import net.neoforged.neoforge.client.event.RegisterClientReloadListenersEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.server.FMLServerStartingEvent;
-import net.neoforged.fml.event.server.FMLServerStoppingEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.event.server.ServerStoppingEvent;
 
 @Mod.EventBusSubscriber(modid = MyMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModInitializer {
@@ -20,13 +20,13 @@ public class ModInitializer {
     private static AssetCacheManager cacheManager;
 
     @SubscribeEvent
-    public static void onServerStarting(FMLServerStartingEvent event) {
+    public static void onServerStarting(ServerStartingEvent event) {
         cacheManager = new AssetCacheManager();
         NeoForge.EVENT_BUS.register(new ResourceManagerListener(cacheManager));
     }
 
     @SubscribeEvent
-    public static void onServerStopping(FMLServerStoppingEvent event) {
+    public static void onServerStopping(ServerStoppingEvent event) {
         cacheManager.shutdown();
     }
 }
