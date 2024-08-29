ad23010cc29ecc262c02b72f84770a4ffb33b677
Thunderrock424242 <masonjoshua8@gmail.com>
2024-08-29 13:26:55 -0400

    added chunk moniter

--- src/main/java/net/mcreator/wildernessoddesyapi/ChunkDiagnostics.java
+++ src/main/java/net/mcreator/wildernessoddesyapi/ChunkDiagnostics.java
@@ -1,46 +1,15 @@
-/*
- * The code of this mod element is always locked.
- *
- * You can register new events in this class too.
- *
- * If you want to make a plain independent class, create it using
- * Project Browser -> New... and make sure to make the class
- * outside net.mcreator.wildernessoddesyapi as this package is managed by MCreator.
- *
- * If you change workspace package, modid or prefix, you will need
- * to manually adapt this file to these changes or remake it.
- *
- * This class will be added in the mod root package.
-*/
 package net.mcreator.wildernessoddesyapi;
 
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.api.distmarker.OnlyIn;
-import net.neoforged.api.distmarker.Dist;
+import net.minecraft.world.level.chunk.LevelChunk;
 
-@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
 public class ChunkDiagnostics {
-	public ChunkDiagnostics() {
-	}
 
-	@SubscribeEvent
-	public static void init(FMLCommonSetupEvent event) {
-		new ChunkDiagnostics();
-	}
+    public static void checkChunkForIssues(LevelChunk chunk) {
+        // Corrected method to check for block entities
+        if (chunk.getBlockEntities().isEmpty()) {
+            System.out.println("Warning: Chunk at " + chunk.getPos() + " has no block entities.");
+        }
 
-	@OnlyIn(Dist.CLIENT)
-	@SubscribeEvent
-	public static void clientLoad(FMLClientSetupEvent event) {
-	}
-
-	@EventBusSubscriber
-	private static class ChunkDiagnosticsForgeBusEvents {
-		@SubscribeEvent
-		public static void serverLoad(ServerStartingEvent event) {
-		}
-	}
+        // Additional checks can be added here
+    }
 }
--- src/main/java/net/mcreator/wildernessoddesyapi/ChunkEventHandler.java
+++ src/main/java/net/mcreator/wildernessoddesyapi/ChunkEventHandler.java
@@ -1,46 +1,23 @@
-/*
- * The code of this mod element is always locked.
- *
- * You can register new events in this class too.
- *
- * If you want to make a plain independent class, create it using
- * Project Browser -> New... and make sure to make the class
- * outside net.mcreator.wildernessoddesyapi as this package is managed by MCreator.
- *
- * If you change workspace package, modid or prefix, you will need
- * to manually adapt this file to these changes or remake it.
- *
- * This class will be added in the mod root package.
-*/
 package net.mcreator.wildernessoddesyapi;
 
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
+import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.world.level.chunk.ChunkAccess;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.api.distmarker.OnlyIn;
-import net.neoforged.api.distmarker.Dist;
+import net.neoforged.neoforge.event.level.ChunkEvent;
 
-@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
 public class ChunkEventHandler {
-	public ChunkEventHandler() {
-	}
 
-	@SubscribeEvent
-	public static void init(FMLCommonSetupEvent event) {
-		new ChunkEventHandler();
-	}
+    @SubscribeEvent
+    public void onChunkLoad(ChunkEvent.Load event) {
+        ChunkAccess chunk = event.getChunk();
+        if (chunk instanceof LevelChunk levelChunk) {
+            ChunkDiagnostics.checkChunkForIssues(levelChunk);
+        }
+    }
 
-	@OnlyIn(Dist.CLIENT)
-	@SubscribeEvent
-	public static void clientLoad(FMLClientSetupEvent event) {
-	}
-
-	@EventBusSubscriber
-	private static class ChunkEventHandlerForgeBusEvents {
-		@SubscribeEvent
-		public static void serverLoad(ServerStartingEvent event) {
-		}
-	}
+    @SubscribeEvent
+    public void onChunkUnload(ChunkEvent.Unload event) {
+        // Handle chunk unload events
+        System.out.println("Chunk unloaded: " + event.getChunk().getPos());
+    }
 }
