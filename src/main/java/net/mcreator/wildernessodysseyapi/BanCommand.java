c586a0f61c6fdfb4d6dbaa18e8175a2b03c425d6
Thunderrock424242 <masonjoshua8@gmail.com>
2024-10-13 23:22:32 -0400

    fixed a warning

--- src/main/java/net/mcreator/wildernessodysseyapi/BanCommand.java
+++ src/main/java/net/mcreator/wildernessodysseyapi/BanCommand.java
@@ -1,4 +1,4 @@
-package net.mcreator.wildernessodysseyapi.command;
+package net.mcreator.wildernessodysseyapi;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
